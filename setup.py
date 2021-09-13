ce9dd8cee39ba05f3acb63ccfde2da2954f70e21
Malte Gotz <malte.gotz@helmholtz-berlin.de>
2021-09-13 11:01:59 +0200

    added versioneer for version from tag

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
+import versioneer
 
 setup(
     name='phoebusalarm',
     python_requires='>=3.5',
-    version='0.2.0',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(include=['phoebusalarm']),
     install_requires=['treelib']
 )
