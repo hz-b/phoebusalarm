8b1b06e59ba4d6a176e5fd5847b235fd2dca23b2
Malte Gotz <malte.gotz@helmholtz-berlin.de>
2022-09-01 14:29:56 +0200

    move metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-from setuptools import setup, find_packages
-import os
+from setuptools import setup
 import versioneer
 
-this_directory = os.path.split(os.path.abspath(__file__))[0]
-
-with open(os.path.join(this_directory, "README.md"), "r") as readme:
-    long_description = readme.read()
-
-setup(
-    name="phoebusalarm",
-    python_requires=">=3.5",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="Python Utilities for Phoebus",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Malte Gotz",
-    author_email="malte.gotz@helmholtz-berlin.de",
-    url="https://gitlab.helmholtz-berlin.de/acs/pyphoebus/",
-    license="GPL v3",
-    packages=find_packages(include=["phoebusalarm"]),
-    entry_points={"console_scripts": ["alh-to-xml=phoebusalarm.alh_to_xml:alh_to_xml"]},
-)
+setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
